baec3e20647bbe8df872dd1604dd06fd85f5f107
wplct <wplct@qq.com>
2019-05-08 18:07:27 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='yzs-work',
+    version='0.1',
+    description='yzs work environment',
+    url='https://github.com/wplct/yzs-work',
+    author='wplct',
+    author_email='wplct1@gmail.com',
+    license='Anti-996-License',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Anti-996-License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='yzs work environment',
+    packages=[],
+)
