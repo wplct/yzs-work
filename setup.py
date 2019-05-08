77deea7987266973249487afbdc5ba3088540fcd
wplct <wplct@qq.com>
2019-05-08 19:50:45 +0800

    测试发布

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='yzs-work',
-    version='0.1.1',
+    version='0.1.2',
     description='yzs work environment',
     url='https://github.com/wplct/yzs-work',
     author='wplct',
