011c72669bd5504c6be85ad54765d3a45a7f1d03
wplct <wplct@qq.com>
2019-05-23 12:36:11 +0800

    更新引用方式

--- yzs/__init__.py
+++ yzs/__init__.py
@@ -1,2 +1,16 @@
+from yzs.django_extend.base_choice import Choice
+from yzs.django_extend.base_model import BaseModel
+from yzs.django_extend.base_test_case import BaseTestCase
+from yzs.tastypie_extend.base_resource import api_view, BaseModelResource
+from yzs.tastypie_extend.response_code import ResourceCode, resource_code_manage
 
 
+__all__ = [
+    'Choice',
+    'api_view',
+    'BaseModelResource',
+    'ResourceCode',
+    'resource_code_manage',
+    'BaseTestCase',
+    'BaseModel',
+]
